67e3b3de886500fa4d2ae6686e9561863240aeb9
Andres Roget <andresroget@gmail.com>
2016-07-06 12:01:17 -0400

    update package.json

--- src/app/app.providers.ts
+++ src/app/app.providers.ts
@@ -1,4 +1,4 @@
-import { ELEMENT_PROBE_PROVIDERS } from '@angular/platform-browser';
+// import { ELEMENT_PROBE_PROVIDERS } from '@angular/platform-browser';
 import { HTTP_PROVIDERS } from '@angular/http';
 import { APP_ROUTER_PROVIDERS } from './app.routes';
 import { provideForms } from '@angular/forms';
@@ -8,9 +8,9 @@ import { credit, notifications } from './reducers';
 
 const ENV_PROVIDERS = [];
 
-if (process.env.ENV !== 'build') {
-  ENV_PROVIDERS.push(ELEMENT_PROBE_PROVIDERS);
-}
+// if (process.env.ENV !== 'build') {
+//   ENV_PROVIDERS.push(ELEMENT_PROBE_PROVIDERS);
+// }
 
 ENV_PROVIDERS.push([
     ...HTTP_PROVIDERS,
